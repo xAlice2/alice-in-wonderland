2efc7e9b13aeb82c27910d58136f94b3bcaa27d5
xAlice2 <helena.wu4@gmail.com>
2022-07-26 17:11:08 -0700

    draw player

--- js/main.js
+++ js/main.js
@@ -2,24 +2,38 @@
     document.addEventListener('DOMContentLoaded', main);
 
     function main() {
-        const canvas = document.getElementById("game");
+        const canvas = document.getElementById("game__container"); // Grabbing the canvas and setting/grabbing the context
         const ctx = canvas.getContext('2d');
-        const movement = document.getElementById('movement');
-        const movement2 = document.getElementById('movement-2');
-        const score = document.getElementById('scoreTest');
+        canvas.height = window.innerHeight;
+        canvas.width = window.innerWidth;
+
+
+        // const movement = document.getElementById('movement');
+        // const movement2 = document.getElementById('movement-2');
+        const score = document.getElementById('score');
         const status = document.getElementById('status');
         const reset = document.getElementById('reset');
+        const start = document.getElementById('start');
         let scoreValue = 0;
         score.textContent = scoreValue;
-        const blocks = document.createElement('img');
-        const mushroom = document.createElement('img');
-        const heart = document.createElement('img');
-        blocks.setAttribute('src', 'img/icons8-block-100.png');
-        mushroom.setAttribute('src', 'img/icons8-amanita-100.png');
-        heart.setAttribute('src', 'img/icons8-heart-100.png');
+
+        // const blocks = document.createElement('img');
+        // const mushroom = document.createElement('img');
+        // const heart = document.createElement('img');
+        // blocks.setAttribute('src', 'img/icons8-block-100.png');
+        // mushroom.setAttribute('src', 'img/icons8-amanita-100.png');
+        // heart.setAttribute('src', 'img/icons8-heart-100.png');
+
+        let player;
+        const tailCount = 1; // to keep track of how many blocks the player has
+        const mouse = {
+            x: undefined,
+            y: undefined,
+        }
+
 
         let entities = [];
-        let projectiles = [];
+        let boxes = [];
 
 
         
@@ -30,39 +44,132 @@
         let spacePressed = false;   // space for pause
 
     // ====================== PAINT INTIAL SCREEN ======================= //
-    
+
+    // Filling in the canvas background with a black rectangle
+
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
 
     // EVENT LISTENERS
-
     reset.addEventListener('click', restartGame, false);
 
+    start.addEventListener('click', function() {
+        snake = new Player();
 
-    function keyUpHandler(event) {
-        if(event.code == 'KeyW') {
-            wPressed = false;
-        } else if (event.code === 'KeyA') {
-            aPressed = false;
-        } else if (event.code === 'KeyS') {
-            sPressed = false;
-        } else if (event.code === 'KeyD') {
-            dPressed = false;
-        }
-    }
+    });
+    
+    canvas.addEventListener('mousemove', function(event) {      // grabs the mouse position 
+        mouse.x = event.x;
+        mouse.y = event.y;
+        console.log(mouse.x, mouse.y);
+    });
+    
+
 
     // ====================== SETUP FOR CANVAS RENDERING ======================= //
     // 2D rendering context for canvas element.
     // It is used for drawing shapes, text, images, and other objects.
 
-    canvas.setAttribute('height', window.innerHeight);
-    canvas.setAttribute('width', window.innerWidth);
-    ctx.height = canvas.height;
-    ctx.width = canvas.width;
+        window.addEventListener('resize', function() {
+            canvas.width = window.innerWidth;
+            canvas.height = window.innerHeight;
+        });
 
     
     
     // ====================== ENTITIES ======================= //
 
+// Setting the gravity to be applied to the Player on the y axis
+// const gravity = 0.05
+
+// Setting up Player Class
+class Player {
+    constructor() {
+        this.position = {
+            x: 100,
+            y: 100
+        },
+        this.width = 100
+        this.height = 100
+        // Initializing velocity at 0
+        this.velocity = {
+            x: 0,
+            y: 0
+        }
+    }
+
+    draw() {
+        // Drawing Player
+        ctx.fillStyle = 'red'
+        ctx.fillRect(
+            this.position.x, 
+            this.position.y, 
+            this.width, 
+            this.height
+            )
+    }
+
+    update() {
+        // Update runs in the recursive animate function so it will constantly update the Player on the canvas 
+        this.draw()
+
+        // Players position on the y axis is increased by the velocity 
+        this.position.y += this.velocity.y
+
+        // Players position on the x axis is increased by the velocity 
+        this.position.x += this.velocity.x
+
+        // If Players position on the y axis plus the players height (which will give us the bottom of the Player) plus the velocity of the player on the y axis is less than the canvas height then add gravity, else set velocity equal to zero, effectively stopping the player from falling through the bottom of the canvas.
+        if (this.position.y + this.height + this.velocity.y <= canvas.height)
+        {this.velocity.y += gravity
+        } else {
+            this.velocity.y = 0
+        }
+    }
+}
+
+    // entities.push(snake);
+    const snake = new Player({
+        position: {
+            x: 0,
+            y: 0
+        },
+        velocity: {
+            x: 0,
+            y: 0
+        },
+        offset: {
+            x: 1,
+            y: 1
+        }
+    })
+
     // ====================== HELPER FUNCTIONS ======================= //
+
+    function addNewShrek() {
+        if (shrek.alive) {
+            shrek.alive = false;
+            setTimeout(function() {
+                entities.pop();
+                let x = Math.floor(Math.random() * canvas.width) - 40;
+                let y = Math.floor(Math.random() * canvas.height) - 80;
+                shrek = new Crawler(x, y, 'green', 75, 100, 'ogre');
+                shrek.alive == true;
+                // shrek.setMovement();
+                entities.push(shrek);
+            }, 3000);
+            return true;
+        }
+    }
+
+    function animate() {
+        window.requestAnimationFrame(animate)
+        console.log('this works')
+    }
+
+
+    // add snake tails function on collision
+    // remove snake tails on collision
+
 
     function restartGame() {
         if (endGame) {
@@ -79,6 +186,9 @@
         }
     }
     
+
+    
+
     //  GUI
     
     //  KEYBOARD INTERACTION LOGIC
